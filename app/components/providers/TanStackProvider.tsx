63289c4e297244ff07e381590c84321662076ea1
gcs-abdulwahab <100867378+gcs-abdulwahab@users.noreply.github.com>
2023-09-20 11:15:17 +0500

    TanStack React Query  and Demo.tsx done

---
+++ app/components/providers/TanStackProvider.tsx
@@ -0,0 +1,23 @@
+"use client"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useState } from "react";
+
+const TanStackProvider = ( {children}:{children:React.ReactNode}    ) => {
+  
+  // using useState for queryClient
+  const [queryClient] = useState(() => new QueryClient());
+
+
+	return (
+    <QueryClientProvider client={queryClient}>
+      
+      {children}
+
+
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
+	);
+};
+
+export default TanStackProvider;
