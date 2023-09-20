72244e7a9920daa2252e4a3d7450cf0dcaafc8d1
gcs-abdulwahab <100867378+gcs-abdulwahab@users.noreply.github.com>
2023-09-20 11:16:04 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
-import Block from "./components/Block";
 import Hero from "./components/Hero";
-import Feature from "./components/ui/Feature";
-import FeatureBlock from "./components/ui/FeatureBlock";
+
 
 export default function Home() {
 
   return (
     <>
       <Hero />
-      <Feature />
+
     </>
   );
 }
